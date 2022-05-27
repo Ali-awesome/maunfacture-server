28935516db55005afff540b257bc124b74cdde4c
ALI <68592145+Ali-awesome@users.noreply.github.com>
2022-05-28 03:33:34 +0600

    some changes and review api

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sqfg1.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -17,10 +32,11 @@ async function run() {
     try {
         await client.connect();
         const toolCollection = client.db('dbTools').collection('tool');
+        const userCollection = client.db('dbTools').collection('user');
 
         app.get('/tools', async (req, res) => {
             const query = {};
-            const result = await toolCollection.find(query).toArray();
+            const result = await toolCollection.find(query).sort({ _id: -1 }).toArray();
             res.send(result)
         });
         app.get('/tools/:id', async (req, res) => {
@@ -30,12 +46,24 @@ async function run() {
             res.send(result);
         });
 
-        app.post('/login', async (req, res) => {
+        app.put('/user/:email', async (req, res) => {
+
+            const email = req.params.email;
             const user = req.body;
-            const accessToken = jwt.sign(user, process.env.SECRET_KEY, {
-                expiresIn: '1d'
-            });
-            res.send({ accessToken });
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateUser = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateUser, options);
+            const token = jwt.sign({ email: email }, process.env.SECRET_KEY, { expiresIn: '1y' })
+            res.send({ result, token });
+        });
+
+        app.post('/review', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
         })
 
     }
